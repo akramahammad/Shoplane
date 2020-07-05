678e9c6d7d613b248c48ece84476a684b365b15b
akramahammad <65407507+akramahammad@users.noreply.github.com>
2020-07-05 14:02:19 +0530

    Update checkout.js

--- checkout.js
+++ checkout.js
@@ -20,17 +20,20 @@ $(document).ready(function() {
         $("#totitems").text(prods.length);
         prodcount.text(tot);
     }
+    if (prodcount.text() == 0 || $("#totitems").text() == 0) {
+        $("#totpricediv").css({ display: "none" })
+    }
     var disp = false;
     $(".fa-bars").click(function() {
         if (disp == false) {
             $(this).removeClass("fa-bars");
             $(this).addClass("fa-times");
-            $("#dropdown").css({ display: "block" });
+            $("#dropdown").css({ height: "165px", padding: "10px 0" });
             disp = true;
         } else {
             $(this).removeClass("fa-times");
             $(this).addClass("fa-bars");
-            $("#dropdown").css({ display: "none" });
+            $("#dropdown").css({ height: "0", padding: "0" });
             disp = false;
         }
     })
@@ -45,23 +48,121 @@ $(document).ready(function() {
         array = JSON.stringify(array);
         localStorage.setItem("items", array);
         prodcount.text("0");
+        alert("Order Placed Successfully");
     })
+
 
     function createcartcard(image, title, qty, price) {
         var cartitem = $("<div>").addClass("cartitem");
         var cartimg = $("<img>").attr("src", image);
         var itemcont = $("<div>").addClass("cartcontent");
         var tit = $("<h4>").text(title);
-        var qntytxt = $("<p>").text("Qty: ");
+        var qtychangediv = $("<div>").addClass("qtychngediv");
+        var plusdiv = $("<div>");
+        var plusicon = $("<i>").addClass("fas fa-plus");
+        var minusdiv = $("<div>");
+        var minusicon = $("<i>").addClass("fas fa-minus");
+        var qntytxt = $("<p>").text("x ");
         var qnty = $("<span>").text(qty);
+        var amtdeldiv = $("<div>").addClass("amtdeldiv");
+        var delicon = $("<i>").addClass("fas fa-trash");
         var amttxt = $("<p>").addClass("itemprice").text("Amount: Rs ");
         var tot = qty * price;
         var amt = $("<span>").text(tot);
-        amttxt.append(amt);
+        plusdiv.append(plusicon);
+        minusdiv.append(minusicon);
         qntytxt.append(qnty);
-        itemcont.append(tit, qntytxt, amttxt);
+        amttxt.append(amt);
+        amtdeldiv.append(amttxt, delicon);
+        qtychangediv.append(minusdiv, qntytxt, plusdiv);
+        itemcont.append(tit, qtychangediv, amtdeldiv);
         cartitem.append(cartimg, itemcont);
+
+        delicon.click(function() {
+            cartitem.remove();
+            totamt = totamt - tot;
+            $("#totprice").text("Rs " + totamt);
+            var totitems = $("#totitems").text() - 1;
+            $("#totitems").text(totitems);
+            var cnt;
+            var modarr = JSON.parse(localStorage.getItem("items"));
+            for (var i = 0; i < modarr.length; i++) {
+                if (modarr[i].name == title) {
+                    cnt = modarr[i].count;
+                    modarr.splice(i, 1);
+                    break;
+                }
+            }
+            var oldcnt = prodcount.text();
+            var newcnt = oldcnt - cnt;
+            prodcount.text(newcnt);
+            modarr = JSON.stringify(modarr);
+            localStorage.setItem("items", modarr);
+        })
+
+        plusdiv.click(function() {
+            qty++;
+            totamt = totamt - tot;
+            tot = qty * price;
+            amt.text(tot);
+            qnty.text(qty);
+            totamt = totamt + tot;
+            $("#totprice").text("Rs " + totamt);
+
+            var cnt;
+            var modarr = JSON.parse(localStorage.getItem("items"));
+            for (var i = 0; i < modarr.length; i++) {
+                if (modarr[i].name == title) {
+                    cnt = modarr[i].count;
+                    cnt++;
+                    modarr[i].count = cnt;
+                    break;
+                }
+            }
+            var cntold = prodcount.text();
+            cntold++;
+            prodcount.text(cntold);
+            modarr = JSON.stringify(modarr);
+            localStorage.setItem("items", modarr);
+
+        })
+
+        minusdiv.click(function() {
+            if (qty > 1) {
+                qty--;
+                totamt = totamt - tot;
+                tot = qty * price;
+                amt.text(tot);
+                qnty.text(qty);
+                totamt = totamt + tot;
+                $("#totprice").text("Rs " + totamt);
+                var cnt;
+                var modarr = JSON.parse(localStorage.getItem("items"));
+                for (var i = 0; i < modarr.length; i++) {
+                    if (modarr[i].name == title) {
+                        cnt = modarr[i].count;
+                        cnt--;
+                        modarr[i].count = cnt;
+                        break;
+                    }
+                }
+                var cntold = prodcount.text();
+                cntold--;
+                prodcount.text(cntold);
+                modarr = JSON.stringify(modarr);
+                localStorage.setItem("items", modarr);
+
+
+            }
+        })
+
         return cartitem;
     }
+
+
+
+
+
+
 
 })
